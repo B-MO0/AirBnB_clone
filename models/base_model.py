8b21b3f13ee1df8179dbc3addb6af38f00adfa0c
B-MO0 <144631945+B-MO0@users.noreply.github.com>
2024-03-07 01:26:53 +0100

    1111

--- models/base_model.py
+++ models/base_model.py
@@ -4,10 +4,18 @@ from time import sleep
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+    def __init__(self, *args, **kwargs):
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ("created_at", "updated_at"):
+                        value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now().isoformat()
+            self.updated_at = datetime.now().isoformat()
 
     def __str__(self):
         return f"[ {self.__class__.__name__} ] ({self.id}) {self.__dict__}"
@@ -20,9 +28,9 @@ class BaseModel:
         data["__class__"] = self.__class__.__name__
         return data
 
-my_model = BaseModel()
+my_model = BaseModel(id='58079290-50e6-41aa-80b3-4d94e7ed5d99', created_at='2025-03-07T01:05:24.178672', updated_at='2025-03-07T01:05:24.178672')
 my_model2 = BaseModel()
 
 #TESTING
-print(my_model.__str__())
+print(my_model.__dict__)
 
