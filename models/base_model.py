d00b0b8b1bcd9c28c49e5562c1fed0cb17539639
mohamed ballouchi <contact.mohamedballouchi@gmail.com>
2024-03-10 16:32:18 +0000

    gg

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ("created_at", "updated_at"):
-                        value = datetime.fromisoformat(value)
+                        value = datetime()
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
